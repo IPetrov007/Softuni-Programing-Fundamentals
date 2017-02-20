307ec43be5d446e61a52bb6a2f07d36fd26f5347
Ivan Petrov <ipp007@gmail.com>
2017-02-20 22:05:26 +0200

    new version of 06_WineCraft

--- ListExercise2/06_WineCraft/06_WineCraft/Program.cs
+++ ListExercise2/06_WineCraft/06_WineCraft/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _06_WineCraft
 {
@@ -10,57 +11,78 @@ namespace _06_WineCraft
         static void Main(string[] args)
         {
             List<int> grapes = Console.ReadLine()
-                .Split(' ')
-                .Select(int.Parse)
-                .ToList();
+                   .Split(' ')
+                   .Select(int.Parse)
+                   .ToList();
             int growthDays = int.Parse(Console.ReadLine());
-            
-            while (grapes.Count >= growthDays)
+
+            while (grapes.Count > growthDays)
             {
-                int days = 0;
-                GrowingGrapes(grapes, growthDays, days);
+
+                for (int currentRound = 0; currentRound < growthDays; currentRound++)
+                {
+                    IncrementAllGrapes(grapes);
+
+
+                    for (int i = 0; i < grapes.Count; i++)
+                    {
+                        GrowingForDay(grapes, i);
+                    }
+
+
+                }
                 for (int i = 0; i < grapes.Count; i++)
                 {
                     if (grapes[i] <= growthDays)
                     {
-                        grapes.Remove(grapes[i]);
+                        grapes.RemoveAt(i);
+                        i--;
                     }
                 }
-            }
 
+            }
             Console.WriteLine(string.Join(" ", grapes));
         }
 
-        private static List<int> GrowingGrapes(List<int> grapes, int growthDays, int days)
+        private static void IncrementAllGrapes(List<int> grapes)
         {
-            while (days < growthDays)
+            for (int i = 0; i < grapes.Count; i++)
             {
-                for (int i = 0; i < grapes.Count; i++)
-                {
-                    grapes[i]++;
-                }
-                for (int j = 1; j < grapes.Count - 1; j++)
-                {
-                    if ((grapes[j] > grapes[j - 1]) && (grapes[j] > grapes[j + 1]))
-                    {
-                        grapes[j]--;
-                        if (grapes[j - 1] > 0)
-                        {
-                            grapes[j]++;
-                        }
-                        if (grapes[j + 1] > 0)
-                        {
-                            grapes[j]++;
-                        }
-                        grapes[j - 1] = Math.Max(grapes[j - 1] - 2, 0);
-                        grapes[j + 1] = Math.Max(grapes[j + 1] - 2, 0);
+                grapes[i]++;
+            }
+        }
 
+        private static void GrowingForDay(List<int> grapes, int i)
+        {
+            var isFirstElement = i == 0;
+            var isLastElement = i == grapes.Count - 1;
+
+            if (!isFirstElement && !isLastElement)
+            {
+                var previosIndex = i - 1;
+                var nextIndex = i + 1;
+
+                var isGreaterThanPrevious = grapes[i] > grapes[previosIndex];
+                var isGraterThanNext = grapes[i] > grapes[nextIndex];
+
+                var isGraterGrape = isGreaterThanPrevious && isGraterThanNext;
+
+                if (isGraterGrape)
+                {
+                    grapes[i]--;
+
+                    if (grapes[previosIndex] > 0)
+                    {
+                        grapes[i]++;
+                        grapes[previosIndex] = Math.Max(grapes[previosIndex] - 2, 0);
+                    }
+                    if (grapes[nextIndex] > 0)
+                    {
+                        grapes[i]++;
+                        grapes[nextIndex] = Math.Max(grapes[nextIndex] - 2, 0);
                     }
                 }
-                days++;
             }
-
-            return grapes;
         }
     }
 }
